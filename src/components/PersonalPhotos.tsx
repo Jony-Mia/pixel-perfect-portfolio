014304c6c30fbb41459d3cb85f4c53fec55f72a4
Jony-Mia <ajonymia321@gmail.com>
2026-02-08 22:56:59 +0600

    personal photo added and remove previous

--- src/components/PersonalPhotos.tsx
+++ src/components/PersonalPhotos.tsx
@@ -7,12 +7,17 @@ gsap.registerPlugin(ScrollTrigger);
 
 // Placeholder personal photos - you can replace with actual photos
 const personalPhotos = [
-  { src: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=400&h=400&fit=crop', alt: 'Personal Photo 1' },
-  { src: 'https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?w=400&h=400&fit=crop', alt: 'Personal Photo 2' },
-  { src: 'https://images.unsplash.com/photo-1560250097-0b93528c311a?w=400&h=400&fit=crop', alt: 'Personal Photo 3' },
-  { src: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=400&h=400&fit=crop', alt: 'Personal Photo 4' },
-  { src: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=400&h=400&fit=crop', alt: 'Personal Photo 5' },
-  { src: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=400&h=400&fit=crop', alt: 'Personal Photo 6' },
+  { src: 'https://scontent.fdac187-1.fna.fbcdn.net/v/t39.30808-1/502376387_580222371795217_7468370731717751658_n.jpg?stp=c111.111.858.858a_dst-jpg_s200x200_tt6&_nc_cat=106&ccb=1-7&_nc_sid=1d2534&_nc_ohc=WGYKx9PIxRkQ7kNvwHU-Jv6&_nc_oc=AdkcWob1YxN6kBD70r0_OiI9c6xwGVY6-70B2vpr3GsdGp5i0591fm-QC9PNvYMBd3U&_nc_zt=24&_nc_ht=scontent.fdac187-1.fna&_nc_gid=09KROb716gmWScBQ2a5mAg&oh=00_Aft67FxZ3A-QPbZomWDSg_bz29chJrcIdsjQVU_0RMjNzA&oe=698E99B2', alt: 'Personal Photo 1' },
+
+
+  { src: 'https://scontent.fdac187-1.fna.fbcdn.net/v/t39.30808-6/597787973_724445007372952_6547800389915131382_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=KpmOddzwrl0Q7kNvwFTsArJ&_nc_oc=Adn-2o7A1mQLCRkKknRQ9WhnfFBd22zCdPDZzWo92i75XEQItQQ1lG505ZF5pnP54wE&_nc_zt=23&_nc_ht=scontent.fdac187-1.fna&_nc_gid=lZKkxHWIXlHIgvF4PFU8Gg&oh=00_AfvLLbRebndh5ZmhyXQeegHy2Qku5Fs5LFs7wVkm4g6XWw&oe=698E9852', alt: 'Personal Photo 2' },
+
+  { src: 'https://scontent.fdac187-1.fna.fbcdn.net/v/t39.30808-6/550681845_652520447898742_2463063944442090996_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=3qpou9H6uXAQ7kNvwHXL5pX&_nc_oc=AdmkmK6UTryNK4nGLHv04r3hP9DU2b_K7_wXFKrIWVot-G6rh5v-EIa1NDh3IB3IbIU&_nc_zt=23&_nc_ht=scontent.fdac187-1.fna&_nc_gid=8VCpqJrzEMVCRGe6yEJ_nw&oh=00_Afvaberu-sLwdXVwHNZCg17QzQYJCVgSxoxI0J7eUZpB-g&oe=698EA7CC', alt: 'Personal Photo 3' },
+
+  { src: 'https://scontent.fdac187-1.fna.fbcdn.net/v/t39.30808-6/549702308_652524194565034_7131461599180223896_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=5lpA2Z5TwlMQ7kNvwEieJFF&_nc_oc=AdlqkiL7C2ZxgFODTDhMnMG7AqqaEXBBt5LkcYNBqyqiVzjZypWfp73jQIzzuXjPiFQ&_nc_zt=23&_nc_ht=scontent.fdac187-1.fna&_nc_gid=M4y8zoef6o6ldrnQ0NBkwg&oh=00_Aftz7YigctZ6sU4I7ejCQ3kWDYVOJUCSYB_LzJsU2ta2qQ&oe=698EACE9', alt: 'Personal Photo 4' },
+
+  { src: 'https://scontent.fdac187-1.fna.fbcdn.net/v/t39.30808-6/481679477_496968886787233_1013159031139399845_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_ohc=jJSRIxuC3sIQ7kNvwHV24Lk&_nc_oc=AdkPDhzm4qMXhVK68vjwjdRKoNVx4CZE24_Q0pdpP4W4FRX_5g3b766f_XAKeBjY3K0&_nc_zt=23&_nc_ht=scontent.fdac187-1.fna&_nc_gid=Pu9Il6UkkwS8s3MF4jJLzQ&oh=00_AfvGY3x73B9xlOeSCT1zIg-rmXHxMTAJwhyamWd16W_n6A&oe=698E976C', alt: 'Personal Photo 5' },
+  { src: 'https://scontent.fdac187-1.fna.fbcdn.net/v/t39.30808-6/480064557_485220267962095_7558511017176688998_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=_bshKu84nncQ7kNvwEPIKYk&_nc_oc=AdkRonjd9vOSgKswHsR196I9HqPXzzWEzxRXn-dpe_Hn5YPizAXaS0M7Mq3j34EPRCo&_nc_zt=23&_nc_ht=scontent.fdac187-1.fna&_nc_gid=79Vj6yvbM4WG_NCvimRIpQ&oh=00_AfsHD-DUOqvdZcjyzHE0W8kKuEP-ccyvGW-AJi27iznXVg&oe=698E79CD', alt: 'Personal Photo 6' },
 ];
 
 const PersonalPhotos = () => {
